ca0b915118361f9cbed0b3580e15fe32af3e5be9
alifanugrah045 <97005608+alifanugrah045@users.noreply.github.com>
2022-10-30 10:34:33 +0800

    update navbar active

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { emptyCart } from "../redux/cartSlice";
 import { userLogout } from "../redux/userSlice";
 import ButtonComponent from "./ButtonComponent";
 
 const NavbarComponent = (props) => {
+  const location = useLocation(); // once ready it returns the 'window.location' object
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isAuthenticated } = useSelector((state) => state.user);
@@ -31,12 +36,12 @@ const NavbarComponent = (props) => {
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <Link to="/" class="nav-link active" aria-current="page" href="#">
+                    <Link to="/" className={"nav-link" + (url === "/" ? " active" : "")} aria-current="page" href="#">
                       Products
                     </Link>
                   </li>
                   <li class="nav-item">
-                    <Link to="/cart" class="nav-link" href="#">
+                    <Link to="/cart" className={"nav-link" + (url === "/" ? " active" : "")} href="#">
                       Cart
                     </Link>
                   </li>
@@ -67,12 +72,12 @@ const NavbarComponent = (props) => {
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <Link to="/" class="nav-link active" aria-current="page">
+                    <Link to="/" className={"nav-link" + (url === "/" ?" active" : "")} aria-current="page">
                       Products
                     </Link>
                   </li>
                   <li class="nav-item">
-                    <Link to="/rekap-penjualan" class="nav-link" href="#">
+                    <Link to="/rekap-penjualan" className={"nav-link" + (url === "/" ?" active" : "")} href="#">
                       Rekap Penjualan
                     </Link>
                   </li>
